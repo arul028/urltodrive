0a439285cab6c56513e3c7a5c40a32d2c0536567
Dhaval <dhaval.mehta197@gmail.com>
2018-07-06 15:53:27 +0530

    update spring boot version to 2.0.3

--- src/main/java/com/github/dhaval_mehta/savetogoogledrive/SaveToGoogleDriveApplication.java
+++ src/main/java/com/github/dhaval_mehta/savetogoogledrive/SaveToGoogleDriveApplication.java
@@ -3,13 +3,13 @@ package com.github.dhaval_mehta.savetogoogledrive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
-public class SaveToGoogleDriveApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) throws Exception {
+public class SaveToGoogleDriveApplication {
+
+	public static void main(String[] args) {
 		SpringApplication.run(SaveToGoogleDriveApplication.class, args);
 	}
 
