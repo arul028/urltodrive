947b2ec18a82a17ad3434aa35f94d8fa974d80b1
Dhaval Mehta <dhaval.mehta197@gmail.com>
2018-07-09 01:22:00 +0530

    integrate sentry

--- src/main/java/com/github/dhaval_mehta/savetogoogledrive/SaveToGoogleDriveApplication.java
+++ src/main/java/com/github/dhaval_mehta/savetogoogledrive/SaveToGoogleDriveApplication.java
@@ -3,6 +3,9 @@ package com.github.dhaval_mehta.savetogoogledrive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -11,6 +14,16 @@ public class SaveToGoogleDriveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaveToGoogleDriveApplication.class, args);
+	}
+
+    @Bean
+    public ServletContextInitializer sentryServletContextInitializer() {
+        return new io.sentry.spring.SentryServletContextInitializer();
+    }
+
+	@Bean
+	public HandlerExceptionResolver sentryExceptionResolver() {
+		return new io.sentry.spring.SentryExceptionResolver();
 	}
 
 }
